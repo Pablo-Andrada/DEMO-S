7008e6030375801f48cc80999aa197c35e58f4ac
Pablo-Andrada <76768919+Pablo-Andrada@users.noreply.github.com>
2024-12-03 04:28:10 -0300

    creando components

---
+++ src/components/Card.jsx
@@ -0,0 +1,11 @@
+const Card = () => {
+    return (
+       <div>
+            <h1>Andrada Pablo Matias</h1>
+            <h3>email: pabloandrada@gmail.com</h3>
+            <p>City: Pilar, Córdoba, Argentina</p>   
+       </div>
+    )
+}
+
+export default Card;
